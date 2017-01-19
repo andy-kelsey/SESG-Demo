933dd6ff333a957df40fadf596ddb6d12292486c
andy.kelsey <andy.kelsey@plexus.com>
2017-01-19 08:37:08 -0600

    Committing Hello Study Group branch

--- HelloWorld/src/HelloWorld.c
+++ HelloWorld/src/HelloWorld.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello Study Group!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
